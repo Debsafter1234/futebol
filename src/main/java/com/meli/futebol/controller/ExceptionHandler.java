cb1a94d787e20c28b1eb1988d3bc8929e3f2ca5c
debfernandes <debora.mj18@gmail.com>
2024-07-23 13:53:42 -0300

    Alterando o nome da Classe controller de ClubeAdvice para ExceptionHandler

--- src/main/java/com/meli/futebol/controller/ExceptionHandler.java
+++ src/main/java/com/meli/futebol/controller/ExceptionHandler.java
@@ -2,15 +2,12 @@ package com.meli.futebol.controller;
 
 import com.meli.futebol.dto.BaseResponseDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class ClubeAdvice {
+public class ExceptionHandler {
 
-    @ExceptionHandler(ExeptionPersonalizada.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(ExeptionPersonalizada.class)
     public ResponseEntity<BaseResponseDTO> exeptionPersonalizada(ExeptionPersonalizada ex) {
         return ResponseEntity.status(ex.getStatus())
                 .body(new BaseResponseDTO(ex.getStatus(), "erro", ex.getMessage()));
